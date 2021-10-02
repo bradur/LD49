dc5bf31bbd786d4c2492c0f2605e3da5aab35a15
Juutis <Juutis>
2021-10-02 19:41:21 +0300

    Added Bottle Powerup

---
+++ LD49/Assets/Scripts/Items/Powerup.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+
+    private Vector3 origPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        origPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, Time.deltaTime * 180.0f);
+        transform.position = origPosition + Vector3.up * Mathf.Sin(Time.time * 2.0f) * 1.0f;
+    }
+}
