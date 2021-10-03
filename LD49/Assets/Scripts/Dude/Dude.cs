12955f394190ee8daf8371570741414af11210fa
Juutis <Juutis>
2021-10-03 12:33:56 +0300

    Added drinking animation cool stuffs

--- LD49/Assets/Scripts/Dude/Dude.cs
+++ LD49/Assets/Scripts/Dude/Dude.cs
@@ -8,12 +8,18 @@ public class Dude : MonoBehaviour
     [SerializeField]
     private UnityEvent DieEvent;
 
+    [SerializeField]
+    private UnityEvent DrinkEvent;
+
+    private FillableBottle bottle;
+
     private Ragdollizer ragdoll;
 
     // Start is called before the first frame update
     void Start()
     {
         ragdoll = GetComponent<Ragdollizer>();
+        bottle = GetComponentInChildren<FillableBottle>();
     }
 
     // Update is called once per frame
@@ -24,6 +30,20 @@ public class Dude : MonoBehaviour
     public void Slip(BananaSlipEvent slipEvent) {
         ragdoll.Slip(slipEvent.Foot, slipEvent.Banana);
         die();
+    }
+
+    private float drinkAmount = 1.0f;
+
+    public void Drink() {
+        DrinkEvent.Invoke();
+
+        //TODO: get rid of this once it's handled externally
+        drinkAmount = drinkAmount - 0.25f;
+        SetDrinkAmount(drinkAmount);
+    }
+
+    public void SetDrinkAmount(float percentage) {
+        bottle.SetFillAmount(percentage);
     }
 
     private void die() {
---
+++ LD49/Assets/Scripts/Dude/FillableBottle.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FillableBottle : MonoBehaviour
+{
+    [SerializeField]
+    private MeshRenderer rend;
+    
+    [SerializeField]
+    private float minFill = 0.0f;
+    
+    [SerializeField]
+    private float maxFill = 1.0f;
+
+    [SerializeField]
+    private float lerpTime = 1.0f;
+
+    private float lerpStarted = -1.0f;
+    private float targetAmount = 1.0f;
+    private float startAmount = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend.material.SetFloat("_FillAmount", maxFill);
+        Debug.Log(rend.material);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lerpStarted >= 0.0f) {
+            var t = (Time.time - lerpStarted) / lerpTime;
+            t = Mathf.Clamp(t, 0.0f, 1.0f);
+            var fillAmount = Mathf.Lerp(startAmount, targetAmount, t);
+            fillAmount = Mathf.Lerp(minFill, maxFill, fillAmount);
+            rend.material.SetFloat("_FillAmount", fillAmount);
+            Debug.Log(fillAmount);
+        }
+    }
+
+    public void SetFillAmount(float amount) {
+        startAmount = targetAmount;
+        targetAmount = amount;
+        lerpStarted = Time.time;
+    }
+}
